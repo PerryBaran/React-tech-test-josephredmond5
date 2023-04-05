1db1706085ff7ff132e63ae69f159360bf44e610
josephredmond5 <josephredmond5@icloud.com>
2023-04-05 11:03:22 +0100

    first commit for tech test

---
+++ src/components/Search.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useState } from "react";
+import "../styles/search.css";
+
+const Search = () => {
+    return ( 
+        <>
+        <input type={text}></input>
+        </>
+    );
+}
+ 
+
+export default Search ;
