8e8374599f5739820b351c8d558819b700630cc2
josephredmond5 <josephredmond5@icloud.com>
2023-04-09 15:03:23 +0100

    updates

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/app.css";
 import Search from "./Search";
 
 const App = () => {
+  const [searchResults, setSearchResults] = useState();
+
   return (
     <div className="app">
-      <img
-        className="nasa-logo"
-        src="https://cdn.cnn.com/cnnnext/dam/assets/200424060716-nasa-worm-logo.jpg"
-        alt="nasaLogo"
+      <img className="nasa-logo"
+      src="https://cdn.cnn.com/cnnnext/dam/assets/200424060716-nasa-worm-logo.jpg"
+      alt="nasaLogo"
       />
-      <Search />
+      <Search setSearchResults={setSearchResults} />
     </div>
-  );
+    );
 }
 
 export default App;
